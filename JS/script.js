56d31b831c35005131053b13ab2bc80249970835
mfotoni <matheusfernandes.otoni@gmail.com>
2025-10-15 17:46:21 -0300

    added a gif to display the location  weather condition

--- JS/script.js
+++ JS/script.js
@@ -39,11 +39,77 @@ async function searchWeather(e) {
     };
     console.log(currentWeather);
 
+    displayWeather(currentWeather);
+
+    displayGiphy(currentWeather.icon);
+
     return data;
   } catch (error) {
     "Error to search location weather:", error;
   }
 }
 
+// function to display json to dom
+function displayWeather(currentWeather) {
+  const displayDiv = document.querySelector("#display-div");
+
+  // function to clear dom elements from previous search
+  clearDOM();
+
+  const location = document.createElement("p");
+  location.textContent = currentWeather.place;
+  displayDiv.appendChild(location);
+  const conditions = document.createElement("p");
+  conditions.textContent = currentWeather.conditions;
+  displayDiv.appendChild(conditions);
+  const temperature = document.createElement("p");
+  temperature.textContent = currentWeather.temperature + " Degrees";
+  displayDiv.append(temperature);
+  const feelslike = document.createElement("p");
+  feelslike.textContent = currentWeather.feels;
+  displayDiv.appendChild(feelslike);
+  const humidity = document.createElement("p");
+  humidity.textContent = currentWeather.humidity;
+  displayDiv.appendChild(humidity);
+}
+
+function clearDOM() {
+  const nodeList = document.querySelectorAll("p");
+  if (nodeList !== null) {
+    for (let i = 0; 1 < nodeList.length; i++) {
+      nodeList[i].remove();
+    }
+  }
+}
+
+async function displayGiphy(icon) {
+  // const searchForm = document.querySelector("#search").value;
+  // if (searchForm === "") {
+  //   searchForm === "random";
+  // }
+  const img = document.querySelector("img");
+  // let searchTerm = icon;
+
+  try {
+    const response = await fetch(
+      "https://api.giphy.com/v1/gifs/translate?api_key=aomT16zAydIRpBNLEyICwak3iNig8pHF&s=" +
+        icon
+    );
+    console.log("image search...");
+    const giphy = await response.json();
+
+    img.style.display = "";
+    img.src = giphy.data.images.original.url;
+  } catch (error) {
+    console.error("Fetch image error:", error);
+  }
+}
+
 searchBtn.addEventListener("click", searchWeather);
+
+document.addEventListener("DOMContentLoaded", function hideImgFirst() {
+  let firstImg = document.querySelector("img");
+  firstImg.style.display = "none";
+});
+
 getWeather();
